e37e02ebb5608d6779bf8554443b046b3d2f082d
ajaywizdom <ajaykumaramundla1@gmail.com>
2025-03-22 19:22:27 +0530

    add: oidc for github auth

---
+++ oidc/main.tf
@@ -0,0 +1,51 @@
+resource "aws_iam_openid_connect_provider" "demo" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
+
+}
+
+resource "aws_iam_role" "demo" {
+  name = "demo-role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Federated": "arn:aws:iam::024848487830:oidc-provider/token.actions.githubusercontent.com"
+            },
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Condition": {
+                "StringLike": {
+                    "token.actions.githubusercontent.com:sub": "repo:ajaywizdom/demo-project:*"
+                },
+                "StringEquals": {
+                    "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+                }
+            }
+        }
+    ]
+}
+)
+}
+
+resource "aws_iam_role_policy" "demo" {
+  name = "demo_policy"
+  role = aws_iam_role.demo.id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
+})
+}
