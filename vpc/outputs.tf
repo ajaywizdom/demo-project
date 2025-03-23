ae216627865aa46ec98fcc61668ea1227e7c5a48
ajaywizdom <ajaykumaramundla1@gmail.com>
2025-03-24 00:24:20 +0530

    add: infra wf for eks,vpc using modules

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,3 +2,18 @@ output "no_of_azs" {
   value       = data.aws_availability_zones.azs.names
   description = "number of AZs in the region"
 }
+
+output "vpc_id" {
+  value = aws_vpc.demo.id
+  description = "VPC id"
+}
+
+output "private_subnets" {
+  value = aws_subnet.private[*].id
+  description = "Private subnets ids"
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+  description = "Private subnets ids"
+}
